dcdbf8b2ce5b212eeb2daf829d52dc0c5799f835
ElPandaC <MrPandac@gmail.com>
2024-11-24 21:31:27 +0700

    Исправил ошибки в названии

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,25 @@
 mod data_collections;
-mod predicti_system;
+mod predict_system;
 
 use std::io;
 fn main() {
     loop{
         let mut choices = String::new();
-        println!("Выберите действие
-        1. Выбрать символы для работы
-        2. Парсить исторические данные
-        3. Нормализовать данные
-        4. Обучить модель для предсказаний
-        5. Предсказать следующие свечи
-        6. Выход");
+        println!("Выберите действие:
+        1: Парсить исторические данные
+        2: Нормализовать данные
+        3: Обучить модель для предсказаний
+        4: Предсказать следующие свечи
+        Q: Выход");
 
         io::stdin().read_line(&mut choices).expect("Ошибка чтения строки");
 
         match choices.trim() {
-            "1" => select_symbols(),
-            "2" => data_collections::parse_historical_data(),
-            "3" => predicti_system::normalize_data(),
-            "4" => predicti_system::train_model_predictions(),
-            "5" => predicti_system::predict_future_prices(),
-            "6" => break,
+            "1" => data_collections::parse_historical_data(),
+            "2" => predict_system::normalize_data(),
+            "3" => predict_system::train_model_predictions(),
+            "4" => predict_system::predict_future_prices(),
+            "q" => break,
             _ => println!("Некорректный ввод, попробуйте снова."),
         }
     }
