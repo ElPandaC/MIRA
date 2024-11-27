93ba2dc976c7addd80e71d8df0c136f7283dc714
ElPandaC <MrPandac@gmail.com>
2024-11-27 16:56:15 +0700

    Косметические улучшения

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,33 @@
+mod data_collections; // Сбор данных
+mod predict_system; // Модель для предсказания
+mod db_controller; // Контроллер для работы с БД
+
 use config::{Config, File, Environment};
 use serde::Deserialize;
 use std::io;
 
-mod data_collections;
-mod predict_system;
-
-// Загрузка конфига
+// Подключаем нужные части конфига
 #[derive(Debug, Deserialize)]
-struct AppConfig {
-    base_url: BaseUrl,
+pub struct AppConfig {
+    bybit: ByBit,
     base_settings: BaseSettings,
 }
 
 #[derive(Debug, Deserialize)]
-struct BaseUrl {
-    bybit: String,
+pub struct ByBit {
+    base_url_trading: String,
+    api_key: String,
+    api_secret: String,
 }
 
 #[derive(Debug, Deserialize)]
-struct BaseSettings {
+pub struct BaseSettings {
     base_symbol: String,
     base_days_deph: u16,
 }
 
 impl AppConfig {
-    fn from_env() -> Result<Self, config::ConfigError> {
+    pub fn from_env() -> Result<Self, config::ConfigError> {
         let builder = Config::builder()
             .add_source(File::with_name("config/config"))
             .add_source(Environment::with_prefix("APP"));
@@ -48,7 +51,7 @@ fn main() {
                 io::stdin().read_line(&mut choices).expect("Ошибка чтения строки");
 
                 match choices.trim() {
-                    "1" => data_collections::parse_historical_data(),
+                    "1" => data_collections::parse_historical_data(&config),
                     "2" => predict_system::normalize_data(),
                     "3" => predict_system::train_model_predictions(),
                     "4" => predict_system::predict_future_prices(),
